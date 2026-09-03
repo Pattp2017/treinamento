2e63c524fc4f6be584967737fc97cfad52001791
Pattp2017 <pafptp@gmail.com>
2026-09-03 10:37:05 -0300

    Adiciona configuracao publica do Supabase para GitHub Pages

---
+++ supabase-config.js
@@ -0,0 +1,7 @@
+// Configuração pública do Supabase para o front-end.
+// Use somente a URL do projeto e a chave ANON/PUBLISHABLE.
+// Nunca coloque service_role neste arquivo.
+window.SUPABASE_CONFIG = {
+  url: '',
+  anonKey: ''
+};
