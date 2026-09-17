cbfad9675dbb59d560f3a82f8678fa7b3377be4d
Pattp2017 <pafptp@gmail.com>
2026-09-17 08:28:42 -0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,7 +1,8 @@
 // Configuração pública do Supabase para o front-end.
 // Use somente a URL do projeto e a chave ANON/PUBLISHABLE.
 // Nunca coloque service_role neste arquivo.
+
 window.SUPABASE_CONFIG = {
-  url: 'https://hopbviytryryudvqkdhv.supabase.co',
-  anonKey: 'sb_publishable_GFbpvknzJGXB08UszeOCZw_OiEQ4_o2'
+  url: 'https://uofnninqxnsvaemxpwhh.supabase.co',
+  anonKey: 'sb_publishable_kCX0kt1EFYYL0sR3zQHDzg_6rLVRJQ8'
 };
